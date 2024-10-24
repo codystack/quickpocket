b5ae54673f30c935bf26e5a7f0c83771f4da333a
codystack <ebuzzadvert@gmail.com>
2024-10-24 18:28:44 +0100

    Terms page added

---
+++ terms.php
@@ -0,0 +1,43 @@
+<?php
+include "./components/header.php";
+include "./components/navbar.php";
+?>
+
+    <div class="page_wrapper">
+
+        <section class="blog_detail_section">
+            <div class="container container-sm">
+                <div class="blog_head text-center">
+                    <h1>Terms and Conditions</h1>
+                </div>
+                <div class="blog_body">
+                    <h3>Introduction</h3>
+                    <p>These Terms of Service (“Terms”) govern your use of the Quick Pocket mobile application (“App”). By accessing or using the App, you agree to be bound by these Terms. If you do not agree to these Terms, please do not use the App.</p>
+
+                    <h3>App Purpose</h3>
+                    <p>Quick Pocket is a platform that connects lenders and borrowers. The App facilitates the process of loan requests and approvals.</p>
+
+                    <h3>User Accounts</h3>
+                    <p>To use the App, you may be required to create a user account. You are responsible for maintaining the confidentiality of your account information, including your password. You agree to notify Quick Pocket immediately of any unauthorized use of your account or any other security breach.</p>
+
+                    <h3>Limitation of Liability</h3>
+                    <p>Quick Pocket is not liable for any damages or losses arising from the use of the App, including but not limited to property damage, personal injury, or financial loss.</p>
+
+                    <h3>Indemnification</h3>
+                    <p>You agree to indemnify and hold Quick Pocket harmless from any claims, liabilities, damages, or losses arising from your use of the App or your violation of these Terms.</p>
+
+                    <h3>Governing Law</h3>
+                    <p>These Terms shall be governed by and construed in accordance with the laws of federal republic of Nigeria.</p>
+
+                    <h3>Modifications</h3>
+                    <p>Quick Pocket may modify these Terms from time to time. Your continued use of the App after such modifications constitutes your acceptance of the modified Terms.</p>
+
+                    <h3>Contact Information</h3>
+                    <p>If you have any questions about these Terms or the App, please contact Quick Pocket at <a href="mailto:hello@quickpocket.co">hello@quickpocket.co</a>.</p>
+
+                    <p>By using the Quick Pocket App, you agree to these Terms of Service.</p>
+                </div>
+            </div>
+        </section>
+
+<?php include "./components/footer.php"; ?>
