ad7a8d94385b9ef735cb6abcd8fbb70bd875975d
codystack <ebuzzadvert@gmail.com>
2024-10-25 14:52:07 +0100

    account page  added

---
+++ account-delete.php
@@ -0,0 +1,76 @@
+<?php
+include "./components/header.php";
+?>
+
+    <div class="page_wrapper">
+
+        <div class="full_bg">
+
+            <section class="signup_section">
+                <div class="container">
+
+                    <div class="top_part">
+                        <a href="./" class="back_btn"><i class="icofont-arrow-left"></i> Back to home</a>
+                        <a class="navbar-brand" href="./">
+                            <img src="images/logo-dark.svg" alt="logo">
+                        </a>
+                    </div>
+
+                    <div class="form_block">
+                        <div class="form_side">
+                            <div class="section_title">
+                                <span class="title_badge">Account Deletion</span>
+                                <h2><span>Delete</span> account</h2>
+                                <p>Fill all fields so we can get some info about you. And process your account for deletion.</p>
+                            </div>
+                            <form>
+                                <div class="form-group">
+                                    <input type="name" class="form-control" placeholder="Full Name">
+                                </div>
+                                <div class="form-group">
+                                    <input type="email" class="form-control" placeholder="Email">
+                                </div>
+                                <div class="forgate_check">
+                                    <div class="coustome_checkbox">
+                                        <label for="remamber_check">
+                                            <input type="checkbox" id="remamber_check">
+                                            <span class="checkmark"></span>
+                                            I agree to all the <a href="terms" target="_blank">Terms of use</a>
+                                        </label>
+                                    </div>
+                                </div>
+                                <div class="btn_block">
+                                    <button class="btn puprple_btn ml-0">Sign Up Now</button>
+                                    <div class="btn_bottom"></div>
+                                </div>
+                            </form>
+                        </div>
+                        <div class="side_screen">
+                            <div class="dotes_blue"><img src="images/blue_dotes.png" alt="image"></div>
+                            <div class="left_icon">
+                                <img src="images/smallStar.png" alt="image">
+                            </div>
+                            <div class="right_icon">
+                                <img src="images/bigstar.png" alt="image">
+                            </div>
+                            <div class="scrren">
+                                <img src="images/sign_in_screen.png" alt="image">
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </section>
+
+        </div>
+
+    </div>
+
+    <script src="js/jquery.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/aos.js"></script>
+    <script src="js/main.js"></script>
+
+</body>
+
+</html>
